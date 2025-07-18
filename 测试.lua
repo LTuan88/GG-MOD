0084b801e28469427c91fa968a35e3e1e0e54a72
LTuan88 <5t03uanl1@gmail.com>
2025-07-18 20:25:53 +0700

    Add files via upload

---
+++ 测试.lua
@@ -0,0 +1,16 @@
+function Main0()
+SN = gg.choice({
+	 "播放",
+}, nil, "")
+if SN==1 then
+	 HS1()
+end
+FX=0
+end
+
+function HS1()
+gg.playMusic("http://music.163.com/song/media/outer/url?id=518686034.mp3")
+end
+
+
+Main0()
